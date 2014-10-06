b1fc9e8a6960f7815771da51685fe1b5c9255951
Carlos Teolotitla <carlos.teolotitla@contractor.cengage.com>
2014-10-06 18:58:39 -0400

    Initial kata commit

---
+++ kata-challenge/src/test/java/com/kata/project/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+package com.kata.project;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+	@Test
+	public void sillyTest() throws Exception {
+		assertThat(1, is(2));
+	}
+}
